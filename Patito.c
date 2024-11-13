e02adf6b547e841eede3b87c0504c765e09d96ef
Thierry <Thierry_2222@hotmail.com>
2024-11-13 22:16:48 +0000

    23/03/2007 2

--- Patito.c
+++ Patito.c
@@ -1,84 +1,22 @@
 #include<stdio.h>
 #include<conio.h>
+#include<stdlib.h>
 #include<dos.h>
 #include<graphics.h>
 
 int geexbox;
-
-
-graficos()
-{
-	int gdriver = DETECT, gmode, errorcode, i, j, k;
-
-	initgraph(&gdriver, &gmode, "");
-	errorcode = graphresult();
-	if (errorcode != grOk)  /* an error occurred */
-	{
-		printf("Graphics error: %s\n", grapherrormsg(errorcode));
-		printf("Press any key to halt:");
-		getch();
-		return(1);
-
-	}
-	return(0);
-}
-
-
-pinta()
-{
-  setfillstyle(1,0x01);
-  bar(0,0,640,480);
-  setfillstyle(1,0x0B);
-  bar(0,0,22,22);
-    setfillstyle(1,0x0B);
-  bar(200,200,222,222);
-  setfillstyle(1,0x0B);
-  bar(400,400,444,468);
-pieslice(88,88,0,360,8);
-  getch();
-return(0);
-}
-
-
-cuadrados()
-{
-int ciclo,tecla,x=0,y=0;
-while (ciclo!=84)
-{
-  setfillstyle(1,0x0F);
-  bar(x,y,x+22,y+22);
-  delay(22);
-  setfillstyle(1,0x00);
-  bar(x,y,x+22,y+22);
-  y++;
-  x++;
-  if(kbhit())
-    {
-    tecla=getch();
-    ciclo=tecla;
-    printf ("%i",tecla);
-    }
-  if(y==480)
-    {
-    y=-22;
-    }
-  if(y==-23)
-    {
-    y=479;
-    }
-  if(x==640)
-    {
-    x=-22;
-    }
-  if(x==-23)
-    {
-    x=639;
-    }
-}
-line(0,0,getmaxx(),getmaxy());
-getch();
-return(0);
-}
+//Nombre de las tarjetas de video soportadas
+char *dname[] = {"Requiere deteccion",
+                 "CGA",
+                 "MCGA",
+                 "EGA",
+                 "EGA 64K",
+                 "EGA mono",                      /* EGA monocromo */
+                 "IBM 8514",
+                 "Hercules mono",                /* Hercules monocromo */
+                 "AT&T 6300 PC",
+                 "VGA",
+                 "IBM 3270 PC"         };
 
 
 Geexbox()
@@ -145,7 +83,7 @@ Thierry()
 textbackground(0x00);
 clrscr();
 
-textcolor(0x0F); gotoxy(16,24);  printf (" ÜÜÜÜÜÜÜ  Ü    Ü  Ü  ÜÜÜÜÜ  ÜÜÜÜ   ÜÜÜÜ    Ü     Ü    \n"); 
+textcolor(0x0F); gotoxy(16,24); cprintf (" ÜÜÜÜÜÜÜ  Ü    Ü  Ü  ÜÜÜÜÜ  ÜÜÜÜ   ÜÜÜÜ    Ü     Ü    \n"); 
 textcolor(0x09); gotoxy(16,25); cprintf ("    Û     Û    Û  Û  Û      Û   Û  Û   Û   ฿Û   Û฿    \n"); 
 textcolor(0x0B); gotoxy(16,26); cprintf ("    Û     Û    Û  Û  Û      ÛÜÜÜ฿  ÛÜÜÜ฿    ฿Û Û฿     \n"); 
 textcolor(0x0A); gotoxy(16,27); cprintf ("    Û     Û฿฿฿฿Û  Û  Û฿฿฿฿  ÛÛ     ÛÛ        ฿Û฿      \n"); 
@@ -193,12 +131,19 @@ gotoxy(7,4);    cprintf("บ                                                      
 gotoxy(7,5);    cprintf("ศออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ");
 
 gotoxy(7,8);    cprintf("ษอออออออออออออออออออออออออออออออออป");
-gotoxy(7,9);    cprintf("บ Modo de video          CGA      บ");
-gotoxy(7,10);   cprintf("บ Compatible             EGA      บ");
-gotoxy(7,11);   cprintf("บ                        VGA      บ");
-gotoxy(7,12);   cprintf("บ                                 บ");
-gotoxy(7,13);   cprintf("บ Compatibilidad SVGA Activada    บ");
-gotoxy(7,14);   cprintf("ศอออออออออออออออออออออออออออออออออผ");
+gotoxy(7,9);    cprintf("บ Modo de video     CGA           บ");
+gotoxy(7,10);   cprintf("บ Compatible        MCGA          บ");
+gotoxy(7,11);   cprintf("บ                   EGA           บ");
+gotoxy(7,12);   cprintf("บ                   EGA 64K       บ");
+gotoxy(7,13);   cprintf("บ                   EGA mono      บ");
+gotoxy(7,14);   cprintf("บ                   IBM 8514      บ");
+gotoxy(7,15);   cprintf("บ                   Hercules mono บ");
+gotoxy(7,16);   cprintf("บ                   AT&T 6300 PC  บ");
+gotoxy(7,17);   cprintf("บ                   VGA           บ");
+gotoxy(7,18);   cprintf("บ                   IBM 3270 PC   บ");
+gotoxy(7,19);   cprintf("บ                                 บ");
+gotoxy(7,20);   cprintf("บ Compatibilidad SVGA Activada    บ");
+gotoxy(7,21);   cprintf("ศอออออออออออออออออออออออออออออออออผ");
 
 gotoxy(44,8);    cprintf("ษอออออออออออออออออออออออออออออออออป");
 gotoxy(44,9);    cprintf("บ Entradas            Teclado     บ");
@@ -206,13 +151,119 @@ gotoxy(44,10);   cprintf("บ Detectadas          Raton       บ");
 gotoxy(44,11);   cprintf("บ                                 บ");
 gotoxy(44,12);   cprintf("บ                                 บ");
 gotoxy(44,13);   cprintf("บ                                 บ");
-gotoxy(44,14);   cprintf("ศอออออออออออออออออออออออออออออออออผ");
+gotoxy(44,14);   cprintf("บ                                 บ");
+gotoxy(44,15);   cprintf("บ                                 บ");
+gotoxy(44,16);   cprintf("บ                                 บ");
+gotoxy(44,17);   cprintf("บ                                 บ");
+gotoxy(44,18);   cprintf("บ                                 บ");
+gotoxy(44,19);   cprintf("บ                                 บ");
+gotoxy(44,20);   cprintf("บ                                 บ");
+gotoxy(44,21);   cprintf("ศอออออออออออออออออออออออออออออออออผ");
+return(0);
+}
 
-if(kbhit())
-  {
+
+errorgraficos()
+{
+  gotoxy(7,4);    cprintf("ษออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป");
+  gotoxy(7,5);    cprintf("บ                      Prueba lo siguiente:                            บ");
+  gotoxy(7,6);    cprintf("บ                                                                      บ");
+  gotoxy(7,7);    cprintf("บ 1 Instala el archivo Egavga.bgi en tu sistema.                       บ");
+  gotoxy(7,8);    cprintf("บ 2 Copia el archivo Egavga.bgi que se encuentra en la carpeta BGI     บ");
+  gotoxy(7,9);    cprintf("บ   de tu carpeta de instalacion de Turbo C++ en la carpeta donde se   บ");
+  gotoxy(7,10);   cprintf("บ   encuentra el archivo Armegueddon.exe                               บ");
+  gotoxy(7,11);   cprintf("บ                                                                      บ");
+  gotoxy(7,12);   cprintf("ศออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ");
+  gotoxy(7,13);   cprintf("Presiona un tecla para salir.");
   getch();
-  }
+  return(0);
+}
 
+
+detectargraficos()
+{
+//devuelve el valor de la informacion del hardware detectado
+int gdriver,gmode,errorcode;
+
+//detectar driver de graficos y modo
+detectgraph(&gdriver, &gmode);
+
+//leer resultado de la deteccion de graficos
+errorcode=graphresult();
+if (errorcode != grOk)  //Si ocurre un error
+  {
+  clrscr();
+  gotoxy(7,1);    printf("Error al cargar el driver de graficos: ");
+  gotoxy(7,2);    printf("%s", grapherrormsg(errorcode));
+  errorgraficos();
+  exit(1);
+  }
+textcolor(0x0F);
+gotoxy(9,12);   cprintf("%s",dname[gdriver]);
+return(0);
+}
+
+
+iniciargraficos()
+{
+int gdriver = DETECT, gmode, errorcode;
+
+initgraph(&gdriver, &gmode, "");
+errorcode = graphresult();
+if (errorcode != grOk)  //Si ocurre un error
+  {
+  clrscr();
+  gotoxy(7,1);    printf("Error al cargar el driver de graficos: ");
+  gotoxy(7,2);    printf("%s", grapherrormsg(errorcode));
+  errorgraficos();
+  exit(1);
+  }
+return(0);
+}
+
+
+juego()
+{
+int x=0,y=0;
+int paisaje[30][40]={0 ,11,11,11,11,11,11,11,11,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                     0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 };
+
+for(y=0;y<10;y++)
+  {
+  for(x=0;x<10;x++)
+    {
+    setfillstyle(1,paisaje[y][x]);  //se invierte x y y con el array
+    bar(x*16,y*16,x*16+16,y*16+16);
+    }
+  }
 getch();
 return(0);
 }
@@ -223,8 +274,17 @@ main()
 Geexbox();
 Thierry();
 Inicio();
-graficos();
-pinta();
+detectargraficos();
+
+if(kbhit())  //si se presiono una tecla para omitir la presentacion, pedir que se presione una vez mas una tecla para iniciar el juego
+  {
+  getch();
+  }
+
+getch();
+iniciargraficos();
+juego();
+closegraph();
 return(0);
 }
 
